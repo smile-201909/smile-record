1d8eeef3e7813452cc5a8aeaaf1d0eca5c92fce3
mattun <ryu00630@gmail.com>
2019-10-13 11:35:27 +0000

    user signup login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user =current_user
   end
 
   def update
   end
 
   def show
+    @user =User.find(params[:id])
   end
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, address_attributes:[:id, :post, :a_address, :phone])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   has_many :address, dependent: :destroy
+   accepts_nested_attributes_for :address
+
 end
