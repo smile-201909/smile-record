fed4ab08409a0d22aeab953b1512a20f86f1e5b3
hiroshima820 <53898556+hiroshima820@users.noreply.github.com>
2019-10-21 19:45:38 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -23,9 +23,6 @@ Rails.application.routes.draw do
   # get 'administrators/users/:id/edit' => 'administrators#edit', as: 'edit_administrators_user_main'
   # patch 'administrators/users/:id' => 'administrators#update', as: 'update_administrators_user_main'
   # put 'administrators/users/:id' => 'administrators#update'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   # get 'administrators/show'
   # get 'administrators/edit'
   root 'products#index'
@@ -40,14 +37,12 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   get 'thanks/index'
-  get 'admins/top'
 
   resources :users, only: [:index, :edit, :update, :show, :destroy, :create] do
     resources :addresses, only: [:index, :new, :create, :edit, :update, :destroy, :show]
   end
       resources :receipts, only: [:show, :create, :update]
 
-  resources :products
 
 
 
@@ -65,19 +60,12 @@ Rails.application.routes.draw do
 
 
 #開発の便宜上、一旦ネストを外す　ここまで
-  resources :products do
-    resources :discs, only: [:create, :update]
-    resources :songs, only: [:create, :update]
-  end
   resources :labels, only: [:new, :create, :update]
   resources :genres, only: [:new, :create, :update]
   resources :artists, only: [:new, :create, :update]
 
 
 
-  resources :users, only: [:index, :edit, :update, :show, :destroy, :create] do
-    resources :addresses, only: [:index, :new, :create, :edit, :update, :destroy]
-  end
   resources :carts, only: [:index, :create, :update, :destroy]
 
 
